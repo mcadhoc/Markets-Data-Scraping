72d0b9cea2b1540dbe019c7f1b882927ca2c06f8
Michael Corley <michaelacorley@gmail.com>
2020-03-02 14:46:48 -0500

    Mar 2 2020

--- data_scripts/charts.py
+++ data_scripts/charts.py
@@ -14,23 +14,27 @@ df_metals = pd.DataFrame(data, columns=data.columns, index=data.index)
 data = pd.read_csv("../raw_data/yieldcurve.csv", header=0, index_col=0)
 df_yieldcurve = pd.DataFrame(data, columns=data.columns, index=data.index)
 
+data = pd.read_csv("../raw_data/currency.csv", header=0, index_col=0)
+df_currency = pd.DataFrame(data, columns=data.columns, index=data.index)
+
 #%%
-df_energy.plot.line(rot=90)
+df_energy.plot.line(rot=90, title="Wind and Solar ETF's outperorm traditional energy indexes")
 #wind and solar ETF's are based off of equities prices and
 #not commodities prices, like for oil and natural gas ETF's
 #%%
-df_major_indexes.plot.line(rot=90)
+df_major_indexes.plot.line(rot=90, title="Africa hit hard over past 12 months - brief recovery in Brazil")
 
 #%%
-df_metals.plot.line(rot=90)
+df_metals.plot.line(rot=90, title="Palladium rises rapidly over past 12 months in comparison to other industrials and reserve metals")
 
 #%%
 test = df_yieldcurve.iloc[[0, -1]]
 test = test.transpose()
-test.plot.line(table=True)
-
+test.plot.line(table=True, title="Yield curve inversion and parallel shift")
 
 #%%
-df_yieldcurve['10 Yr'].plot.line()
+df_yieldcurve['10 Yr'].plot.line(title="10 year yields fall, pressuring equities prices upwards")
+
 
 # %%
+df_currency['UUP'].plot.line(rot=90, title = "Dollar index strengthening, but at a much lower pace")
--- data_scripts/scrape_data.py
+++ data_scripts/scrape_data.py
@@ -5,13 +5,14 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-end = datetime.datetime(2020,2,25)
+end = datetime.datetime(2020,3,2)
 days = datetime.timedelta(365*3)
 start = end - days
 
 metals = ['GLD','SLV','COPX', 'PALL', 'SLX', 'REMX']
 major_indexes = ['SPY','INDA','MCHI', 'EWZ', 'IEUR', 'AFK']
 energy = ['USO','UNG','KOL', 'TAN', 'FAN']
+currency = ['UUP','FXE']
 
 def scrape_yield_curve():
 
@@ -56,7 +57,28 @@ def scrape_yahoo_prices(stocks, start, end, file_label, format):
     if format == "close_price":
         data['Close'].to_csv(r"./raw_data/"+file_label+".csv")
 
+def scrape_global_bonds():
+    #http://www.worldgovernmentbonds.com/
+
+    url = 'http://www.worldgovernmentbonds.com/'
+
+    r = requests.get(url)
+    html = r.text
+
+    soup = BeautifulSoup(html)
+    table = soup.find('table', {"class": "t-chart"})
+    rows = table.find_all('tr')
+    data = []
+    for row in rows[1:]:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+
+    result = pd.DataFrame(data, columns=['Date','1 Mo', '2 Mo', '3 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr'])
+    result[-252:].to_csv(r'./raw_data/global_bond.csv', index = False)
+
 scrape_yahoo_prices(major_indexes, start, end, "major_indexes", "year-over-year")
 scrape_yahoo_prices(metals, start, end, "metals", "year-over-year")
 scrape_yahoo_prices(energy, start, end, "energy", "year-over-year")
+scrape_yahoo_prices(currency, start, end, "currency", "year-over-year")
 scrape_yield_curve()
